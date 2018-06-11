b95ad9f265b6ef61b95532c27146696904056225
Mangirish Wagle <vaglomangirish@gmail.com>
2018-06-11 13:33:04 -0400

    Adding some UX plus refactoring changes

--- WeatherApp/api/subscribe_api.py
+++ WeatherApp/api/subscribe_api.py
@@ -4,6 +4,7 @@ from flask import Flask, abort, request, render_template, jsonify
 from validate_email import validate_email
 
 from data_access import file_agent, sub_record
+from utils import app_logger
 
 app = Flask(__name__)
 
@@ -23,22 +24,29 @@ def subscribe_weather():
     }
     :return:
     """
+
+    # try:
     # Request with no form data is bad.
+
+    logger = app_logger.AppLogger().get_logger()
+
     if not request.form:
-         print("ERROR: No Form Data")
-         abort(400)
+        logger.error("No Form Data")
+        abort(400)
 
     # Request with either email or city not present is bad.
     if "email" not in request.form or "city" not in request.form:
-         print("ERROR: email or city not present in data")
-         abort(400)
+        logger.error("email or city not present in data")
+        abort(400)
 
     email = request.form["email"]
     if not validate_email(email):
-         print("ERROR: email invalid")
-         abort(400)
+        logger.error("email invalid")
+        abort(400)
 
     city = request.form['city']
+    if len(city) == 0:
+        logger.error("city is empty")
 
     record = sub_record.SubscriptionRecord(email, city)
 
@@ -46,6 +54,9 @@ def subscribe_weather():
     agent.add_record(record)
 
     return "SUCCESS"
+    # except:
+    #    logger.error("Could not register the subscription for user email {0} and city {1}".format(email, city))
+    #    abort(500)
 
 
 @app.route("/funday/getcities", methods=["GET"])
@@ -54,12 +65,25 @@ def get_cities():
     Gets the list of cities.
     :return:
     """
+
+    logger = app_logger.AppLogger().get_logger()
+
     city_json = {}
+    # try:
     with open(__root_path__.path() + "/data_store/top_city_list.json", "r") as cities:
         city_json = json.load(cities)
 
     return jsonify(city_json)
-
+    # except:
+    #    logger.error("Could not retrieve top 100 cities")
+    #    abort(500)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    #try:
+    props_path = __root_path__.path() + "/resources/properties.json"
+    properties = {}
+    with open(props_path, "r") as props:
+        properties = json.load(props)
+    app.run(host='0.0.0.0', port=properties["http_server_port"], debug=True)
+    # except:
+    # app_logger.AppLogger().get_logger().error("Error while starting the web application")
--- WeatherApp/utils/app_logger.py
+++ WeatherApp/utils/app_logger.py
@@ -7,24 +7,24 @@ class AppLogger:
     """
 
     def __init__(self):
-        self.logger = logging.getLogger('AppLogger')
+        self.logger = logging.getLogger('WeatherAppLogger')
         self.logger.setLevel(logging.DEBUG)
 
         # Create file handler
-        file_handler = logging.FileHandler(__root_path__.path() + "/log/app.log")
-        file_handler.setLevel(logging.DEBUG)
+        # file_handler = logging.FileHandler(__root_path__.path() + "/log/app.log")
+        # file_handler.setLevel(logging.DEBUG)
 
         # create console handler
         console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.ERROR)
+        console_handler.setLevel(logging.DEBUG)
 
         # create formatter and add it to the handlers
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        file_handler.setFormatter(formatter)
+        # file_handler.setFormatter(formatter)
         console_handler.setFormatter(formatter)
 
         # add the handlers to the logger
-        self.logger.addHandler(file_handler)
+        # self.logger.addHandler(file_handler)
         self.logger.addHandler(console_handler)
 
     def get_logger(self):
